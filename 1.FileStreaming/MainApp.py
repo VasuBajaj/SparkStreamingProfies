6821f0462aa2c5214703da0ee993b1f42cd04127
Vasu Bajaj <41167055+VasuBajaj@users.noreply.github.com>
2020-11-19 22:31:55 +0530

    Update MainApp.py

--- 1.FileStreaming/MainApp.py
+++ 1.FileStreaming/MainApp.py
@@ -3,13 +3,13 @@ from pyspark.streaming import StreamingContext
 
 
 sc = SparkContext(appName = 'FileStreaming')
-ssc = StreamingContext(sc, batch_duration = 1)
+ssc = StreamingContext(sc, batchDuration = 1)
 
-stream = ssc.QueueStream([sc.parallelize(list(range(1,1000)),3)])
+stream = ssc.queueStream([sc.parallelize(list(range(1,1000)),3)])
 
-data =  stream.map(lambda x: (x, x%10)).countByValue(lambda x,y = x+y)
+data =  stream.map(lambda x: (x%10)).countByValue()
 data.pprint()
 
 ssc.start()
-ssc.awaitTermination()
-ssc.stop(stopSparkContext = True, stopGracefully = True)
+#ssc.awaitTermination()
+ssc.stop(stopSparkContext = True, stopGraceFully = True)
